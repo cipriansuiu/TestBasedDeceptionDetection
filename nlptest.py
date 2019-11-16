2962bd5a412b73476488adc3d190c9b6be1e47b8
cipriansuiu <suiuciprianconstantin@gmail.com>
2019-11-16 14:05:09 +0000

    trying different classifiers out, to see efficiency of bag of words as features

--- nlptest.py
+++ nlptest.py
@@ -1,8 +1,14 @@
+import pickle
+
 import nltk
 import random
 from nltk.corpus import movie_reviews
 import os
 from sklearn.feature_extraction.text import CountVectorizer
+from nltk.classify.scikitlearn import  SklearnClassifier
+from sklearn.naive_bayes import MultinomialNB,GaussianNB,BernoulliNB
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC,LinearSVC,NuSVC
 import time
 
 
@@ -10,7 +16,7 @@ def read_from_directory(dir_name):
     os.chdir(dir_name)
     review_list = []
     for file in os.listdir():
-        f = open(file, 'r')
+        f = open(file, 'r',errors='ignore')
         if f.mode == 'r':
             review_list.append(f.read())
     return review_list
@@ -59,10 +65,8 @@ def get_most_common_words(text_set, number_of_words):
 
 
 def find_features(document):
-    words = get_n_grams_for_sentence(document[0],2)
+    words = set(document)
     features = {}
-    print(word_features)
-    print(get_n_grams_for_sentence(word_features,2))
     for word in word_features:
         features[word] = (word in words)
 
@@ -76,10 +80,49 @@ def get_n_grams_for_sentence(sentence,n):
     return all_grams
 
 
-print(find_features(documents[0]))
-# featuresets = [(find_features(review), truthful) for (review, truthful) in documents]
-# training_set = featuresets[:800]
-# testing_set = featuresets[800:1000]
-# classifier = nltk.NaiveBayesClassifier.train(training_set)
-# print(nltk.classify.accuracy(classifier, testing_set) * 100)
+
+featuresets = [(find_features(review), truthful) for (review, truthful) in documents]
+training_set = featuresets[:800]
+testing_set = featuresets[800:1000]
+classifier_f = open("naivebayes.pickle","rb")
+NB_classifier = pickle.load(classifier_f)
+classifier_f.close()
+
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+
+# GaussianNB_classifier = SklearnClassifier(GaussianNB())
+# GaussianNB_classifier.train(training_set)
+
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+
+
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+
+
+SGDClassifier_classifier = SklearnClassifier(SGDClassifier())
+SGDClassifier_classifier.train(training_set)
+
+SVC_classifier = SklearnClassifier(SVC())
+SVC_classifier.train(training_set)
+
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+
+NuSVC_classifier = SklearnClassifier(NuSVC())
+NuSVC_classifier.train(training_set)
+
+classifiers = [("NB_classifier",NB_classifier)
+    ,("MNB_classifier",MNB_classifier)
+    ,("BernoulliNB_classifier",BernoulliNB_classifier)
+    ,("LogisticRegression_classifier",LogisticRegression_classifier)
+    ,("SGDClassifier_classifier_",SGDClassifier_classifier)
+    ,("SVC_classifier",SVC_classifier)
+    ,("LinearSVC_classifier",LinearSVC_classifier)
+    ,("NuSVC_classifier",NuSVC_classifier)]
+
+for cl in classifiers:
+    print(cl[0]+": " + str(nltk.classify.accuracy(cl[1], testing_set) * 100))
 # classifier.show_most_informative_features(5)
